22f3e9f75dbd0dfbe925eb787c9718b6af0e62e0
dmitry.darevsky <darevsky.dmitry@gmail.com>
2019-12-13 14:04:49 +0300

    add functon timer

--- js/script.js
+++ js/script.js
@@ -1,35 +1,88 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function() {
+  "use strict";
 
-	'use strict'
-	let tab = document.querySelectorAll('.info-header-tab'),
-		info = document.querySelector('.info-header'),
-		tabContent = document.querySelectorAll('.info-tabcontent');
+  //Tabs
 
-	function hideTabContent(a) {
-		for (let i = a; i < tabContent.length; i++) {
-			tabContent[i].classList.remove('show');
-			tabContent[i].classList.add('hide');
-		}
-	}
-	hideTabContent(1);
+  let tab = document.querySelectorAll(".info-header-tab"),
+    info = document.querySelector(".info-header"),
+    tabContent = document.querySelectorAll(".info-tabcontent");
 
-	function showTabContent(b) {
-		if (tabContent[b].classList.contains('hide')) {
-			tabContent[b].classList.remove('hide');
-			tabContent[b].classList.add('show');
-		}
-	}
+  function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("show");
+      tabContent[i].classList.add("hide");
+    }
+  }
+  hideTabContent(1);
 
-	info.addEventListener('click', function (evt) {
-		let target = evt.target;
-		if (target && target.classList.contains('info-header-tab')) {
-			for (let i = 0; i < tab.length; i++) {
-				if (target == tab[i]) {
-					hideTabContent(0);
-					showTabContent(i);
-					break;
-				}
-			}
-		}
-	});
+  function showTabContent(b) {
+    if (tabContent[b].classList.contains("hide")) {
+      tabContent[b].classList.remove("hide");
+      tabContent[b].classList.add("show");
+    }
+  }
+
+  info.addEventListener("click", function(evt) {
+    let target = evt.target;
+    if (target && target.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
+        }
+      }
+    }
+  });
+
+  //Timer
+
+  let deadLine = "2019-12-31";
+
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
+    // hours = Math.floor((t/1000/60/60) % 24)
+    // day = Math.floor((t/(1000*60*60*24)))
+
+    return {
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = document.querySelector(".hours"),
+      minutes = document.querySelector(".minutes"),
+      seconds = document.querySelector(".seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+			let t = getTimeRemaining(endtime);
+			
+			function addZeros(num){
+				if(num <= 9) {
+						return '0' + num;
+				} else return num;
+		};
+
+      hours.textContent = addZeros(t.hours);
+      minutes.textContent = addZeros(t.minutes);
+      seconds.textContent = addZeros(t.seconds);
+
+      if (t.total <= 0) {
+				clearInterval(timeInterval);
+				hours.textContent = '00';
+				minutes.textContent = '00';
+				seconds.textContent = '00';
+      }
+    }
+  }
+
+  setClock("timer", deadLine);
 });
